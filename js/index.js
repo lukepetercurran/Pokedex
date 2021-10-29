4ae4250b9c141b59f29962da36a3a5acd1248609
Luke Curran <luke.peter.curran54@gmail.com>
2021-10-29 14:54:29 -0500

    back to top functionality

--- js/index.js
+++ js/index.js
@@ -139,7 +139,46 @@ function localStoragePokemon(localStorageArrayElementValue) {
         }));
         displayPokemon(pokemon, "userFavoritePokemon")
     }))
-
 }
 
+$("#yourFavoritesNavBarLink").click(function (e) {
+    e.preventDefault();
+    localStorageArray.forEach(pokemon => localStoragePokemon(pokemon))
+    console.log(localStorageArray)
+    $('#pokedex').hide();
+})
+
+//Clear local storage
+$("#clearFavoritesNavBarLink").click(function () {
+    location.reload()
+    localStorageArray = [];
+})
+
+//Back to top button event listeners and javascript
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
